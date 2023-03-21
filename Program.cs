da9a9dce9cba02158478e08e65e8a55b232b8a5b
DAAN <daanmgneni@gmail.com>
2023-03-21 19:22:57 +0530

    Added Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPS
+{
+    class EmpWage
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master");
+        }
+    }
+}
